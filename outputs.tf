f3ea75c602131dd4a880deced1c01a0e8493f0aa
John Morsley <johnmorsley@gmail.com>
2020-05-14 00:03:42 +0100

    Refactoring

--- outputs.tf
+++ outputs.tf
@@ -15,6 +15,10 @@ output "key_name" {
   value = aws_key_pair.this.key_name
 }
 
-output "private_key_pem" {
+output "public_key" {
+  value = tls_private_key.this.public_key_openssh
+}
+
+output "private_key" {
   value = tls_private_key.this.private_key_pem
 }
