9424ed662f91d0b12385d7d28f927b6710be293d
John Morsley <johnmorsley@gmail.com>
2020-10-16 14:52:59 +0100

    Renamed version to terraform

--- examples/simple/terraform.tfvars
+++ examples/simple/terraform.tfvars
@@ -5,5 +5,5 @@
 #      \  / (_| | |  | | (_| | |_) | |  __/\__ \
 #       \/ \__,_|_|  |_|\__,_|_.__/|_|\___||___/
 
-name   = "example"
-folder = "keys"
+name   = "simple-example"
+folder = "simple-keys"
---
+++ terraform.tf
@@ -0,0 +1,24 @@
+#      _______                   __                     
+#     |__   __|                 / _|                    
+#        | | ___ _ __ _ __ __ _| |_ ___  _ __ _ __ ___  
+#        | |/ _ \ '__| '__/ _` |  _/ _ \| '__| '_ ` _ \ 
+#        | |  __/ |  | | | (_| | || (_) | |  | | | | | |
+#        |_|\___|_|  |_|  \__,_|_| \___/|_|  |_| |_| |_|
+
+terraform {
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+  }
+
+  required_version = ">= 0.13"
+
+}
